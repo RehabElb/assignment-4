b9976d4a56941bd3620a015110fd49debb767f5a
ITP-270 <63158011+RehabElb@users.noreply.github.com>
2022-03-09 00:03:18 +0000

    main file added

--- main.py
+++ main.py
@@ -1,49 +1,16 @@
-hairstyles = [
-    "bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk",
-    "flattop"
-]
-prices = [30, 25, 40, 20, 20, 35, 50, 35]
-last_week = [2, 3, 5, 8, 4, 4, 6, 2]
-#calc_total=
-total_price = 0  #creating a variable to take all prices (like a box to keep all values added)
+def append_size(lst):
+  lst.append(len(lst)) # here we are3 adding the len of the list to the end of the same list 
+  return lst
 
-for x in prices:  # x = 30, 25...35 every iteration of the loop will take a value and add it to the total_price variable
-    total_price += x  #adding each element from price list to the existing value
+list_1 = [23, 42, 108]
+# print(append_size(list_1))
 
-average_price = total_price / len(prices)
 
-print('Average Haircut Price:', average_price)
+list_2 = [1,1,2]
+def append_sum(lst):
+    for i in range(3): # this is the range to indicate the number of loop iterations (0, 1, 2)
+        lst.append(lst[-1] + lst[-2])
+    return lst
 
-new_prices = [
-    x - 5 for x in prices
-]  # every value/element of the list prices should be $5 less and create new_prices list
-print(new_prices)
-
-# new_prices2 = []
-# for x in prices:
-#   x -= 5
-#   new_prices2.append(x)
-
-# print(new_prices2)
-
-# print(total_price)
-
-total_revenue = 0
-
-for i in range(len(
-        hairstyles)):  # range(0, 5) --> (0,1,2,3,4) - range(5)--> (0,1,2,3,4)
-    total_revenue += (prices[i] * last_week[i])
-
-print('Total Revenue:', total_revenue)
-
-average_daily_revenue = total_revenue / 7
-
-print('Average Daily Revenue: $', average_daily_revenue)
-
-cuts_under_30 = [
-    hairstyles[i] for i in range(len(hairstyles)) if new_prices[i] < 30
-]
-# i takes its value from range 0 to 8 (a number every iteration of the loop) and is used as index number for the 2 lists (hairstyles and new_prices).
-# if new_prices[i] is less than 30 then I will insert the same index in hairstyles in my new list(cuts_under_30)
-# if the element at the index i in new_prices list is greater than 30 then the correspondant element in hairstyles list will be skipped
-print(cuts_under_30)
+print(append_sum(list_2))
+# print(append_sum(list_1))
